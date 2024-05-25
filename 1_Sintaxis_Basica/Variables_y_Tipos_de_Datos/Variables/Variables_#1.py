863376a9b2a4a55972769bf9db1a8e39017b54d8
Luis Jimenez <luisalbertojimenez95@gmail.com>
2024-05-24 20:50:23 -0500

    Variables & tipos de Datos
    Definicion de Variables

---
+++ 1_Sintaxis_Basica/Variables_y_Tipos_de_Datos/Variables/Variables_#1.py
@@ -0,0 +1,19 @@
+##En un codigo Python, las variables se expresan de la siguiente manera
+# x = 2 , esto es una variable, constante, donde el valor seguira siendo dos a menos que se cambie por defecto este valor, con algun operador
+# c = a + b , eso significa que la sumatoria de a mas b, dara el valor de c... veremos esto ahora con un ejemplo.
+
+x = 2
+print(x)
+#Esto imprimi en pantalla la variable "x" cuyo valor, es igual a 2, pasemos con el siguiente ejemplo
+
+b = 13
+c = x + b
+print(c)
+#En este ejemplo damos, como resultado, que c es una variable, que resulta de una operacion entre otras dos variables
+
+#Tambien podemos acordar el codigo, para una mejor visualizacion
+print(x+b)
+#Ojo, en este ejemplo, no asignamos el valor a "c", unicamente imprimimos el valor de dicha operación
+
+
+#Existen diferentes tipos de datos para las variables, que iremos encontrando en este repositorio
---
+++ 1_Sintaxis_Basica/Variables_y_Tipos_de_Datos/Variables/Variables_#2.py
@@ -0,0 +1,12 @@
+#Este es otro ejempo, de como se usan las varibales, en el mundo ya existenv valores definidos, este caso seria PI
+#Para este ejemplo, importaremos la libreria math, que viene por defecto en Python
+#Nota : Ya veremos mas adelante, el uso de librerias, pero como ejemplo de este caso de variables, lo ponemos en practica
+
+
+#Importamos la libreria math
+import math
+
+print(math.pi)
+#Aqui no declaramos una variable para dicho valor, ya que este una constante definida, si ocupamos dicho valor, es necesario usarlo como se muestra "math.pi"
+
+print(math.pi * 5)
